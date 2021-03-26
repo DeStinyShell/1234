ad35117cf230186e0774dd79fb0619047b5d9055
Dmitriy <dmitriy4ever97@gmail.com>
2021-03-26 16:10:43 +0200

    Added greetings

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello mates!");
+        System.out.println("Hello mates!");
+        System.out.println("Hello mates!");
+        System.out.println("Hello mates!");
+        System.out.println("Hello mates!");
     }
 }
